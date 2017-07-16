9b22b92ef623779e3d25f15f1839b4f78e167a60
Eman Fu <emanfu@gmail.com>
2017-07-15 19:12:03 -0700

    Simply use "webView" as screenClass, since for Cordova, there is only one UIView.

--- src/ios/FirebaseAnalyticsPlugin.m
+++ src/ios/FirebaseAnalyticsPlugin.m
@@ -54,7 +54,7 @@
 - (void)setCurrentScreen:(CDVInvokedUrlCommand *)command {
     NSString* name = [command.arguments objectAtIndex:0];
 
-    [FIRAnalytics setScreenName:name screenClass:nil];
+    [FIRAnalytics setScreenName:name screenClass:@"webView"];
 
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
